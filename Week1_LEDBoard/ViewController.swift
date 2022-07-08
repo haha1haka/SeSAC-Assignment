d88c5fe41dc79d0634ccd7ff8a922b6ee66427c4
haha1haka <haha1haka@icloud.com>
2022-07-08 17:38:54 +0900

    refector: AutoLayout추가 and tapGesture

--- Week1_LEDBoard/ViewController.swift
+++ Week1_LEDBoard/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var textFieldView: UIView!
     @IBOutlet weak var mainTextField: UITextField!
     
     @IBOutlet weak var sendButton: UIButton!
@@ -17,48 +18,75 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
+    var colorArray: [UIColor] = [.systemGreen,.darkGray,.red,.yellow,.cyan]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
         self.mainTextField.delegate = self
     }
+    
     func configureUI() {
+        mainTextField.placeholder = "내용을 입력해 주세요"
+        mainTextField.textColor = UIColor.white
+        mainTextField.setPlaceholder(color: .systemGray3)
+        
         resultLabel.textColor = .link
         resultLabel.textAlignment = .center
         resultLabel.font = UIFont.systemFont(ofSize: 44, weight: .heavy)
         
         sendButton.setTitle("전송", for: .normal)
-        sendButton.setTitle("눌러눌렁", for: .highlighted)
-        sendButton.backgroundColor = UIColor.systemGray3
-        sendButton.layer.borderWidth = 1
-        sendButton.layer.borderColor = UIColor.darkGray.cgColor
+        sendButton.setTitle("놔줘", for: .highlighted)
+        sendButton.backgroundColor = UIColor.black
         sendButton.layer.cornerRadius = 8
-        sendButton.tintColor = .systemGreen
+        sendButton.tintColor = .systemBlue
         sendButton.titleLabel?.font = .systemFont(ofSize: 21, weight: .heavy)
         
         textColorButton.setTitle("Color", for: .normal)
-        textColorButton.setTitle("ㅎㅎ", for: .highlighted)
-        textColorButton.backgroundColor = UIColor.systemGray3
-        textColorButton.layer.borderWidth = 1
-        textColorButton.layer.borderColor = UIColor.darkGray.cgColor
+        textColorButton.setTitle("바뀌", for: .highlighted)
+        textColorButton.backgroundColor = UIColor.black
         textColorButton.layer.cornerRadius = 8
-        textColorButton.tintColor = .systemGreen
+        textColorButton.tintColor = .systemBlue
         textColorButton.titleLabel?.font = .systemFont(ofSize: 21, weight: .heavy)
     }
-
     
     @IBAction func sendButtonCliked(_ sender: Any) {
         resultLabel.text = mainTextField.text
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func colorButtonCliked(_ sender: Any) {
+        resultLabel.textColor = colorArray.randomElement()!
     }
     
     
     
+    @IBAction func didEndOnExitTextField(_ sender: Any) {
+        print(#function)
+    }
     
+    @IBAction func tapGesture(_ sender: Any) {
+        if mainTextField.isEditing {
+            self.view.endEditing(true)
+            return
+        } else {
+            textFieldView.isHidden.toggle()
+        }
+    }
 }
 
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
+        resultLabel.text = mainTextField.text
         return true
+    }
+}
+extension UITextField {
+    func setPlaceholder(color: UIColor) {
+        guard let string = self.placeholder else {
+            return
+        }
+        attributedPlaceholder = NSAttributedString(string: string, attributes: [.foregroundColor: color])
     }
 }
