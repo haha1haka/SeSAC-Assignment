46ed81f1f55e1a1c7e87070fe62588d35b16ca16
haha1haka <haha1haka@icloud.com>
2022-07-14 18:48:53 +0900

    [#1] 009회차 Assignment : dummyLabel 에 date 뿌려주기 성공

--- EmotionDiary/EmotionDiary/AnniversaryViewController.swift
+++ EmotionDiary/EmotionDiary/AnniversaryViewController.swift
@@ -12,10 +12,14 @@ class AnniversaryViewController: UIViewController {
     @IBOutlet weak var datePickerView: UIDatePicker!
     @IBOutlet var uiViewCollections: [UIView]!
     @IBOutlet var imageViewCollections: [UIImageView]!
+    @IBOutlet weak var testLabel: UILabel!
+
+    var nowDate: Date?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        datePickerViewAction(datePickerView)
     }
     
     func configureUI() {
@@ -31,6 +35,19 @@ class AnniversaryViewController: UIViewController {
         uiViewCollections.forEach{$0.layer.shadowOpacity = 0.5}
         uiViewCollections.forEach{$0.layer.shadowRadius = 5}
         uiViewCollections.forEach{$0.layer.masksToBounds = false }
+    }
+    
+    
+    @IBAction func datePickerViewAction(_ sender: Any) {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateStyle = .full
+        generateAndApplyDate(formatter)
+    }
+    
+    func generateAndApplyDate(_ formatter: DateFormatter) {
+        nowDate = datePickerView.date
+        testLabel.text = formatter.string(from: nowDate!)
     }
 }
 
