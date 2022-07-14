94d58f0bb094c4420e995262031048f78b0d5663
haha1haka <haha1haka@icloud.com>
2022-07-14 19:12:54 +0900

    [#1] 009회차 Assignment : ValueChanged 에따른 절대시간을 더해 Label 에 날짜 표시 완료

--- EmotionDiary/EmotionDiary/AnniversaryViewController.swift
+++ EmotionDiary/EmotionDiary/AnniversaryViewController.swift
@@ -14,6 +14,11 @@ class AnniversaryViewController: UIViewController {
     @IBOutlet var imageViewCollections: [UIImageView]!
     @IBOutlet weak var testLabel: UILabel!
 
+    @IBOutlet weak var later100Label: UILabel!
+    @IBOutlet weak var later200Label: UILabel!
+    @IBOutlet weak var later300Label: UILabel!
+    @IBOutlet weak var later400Label: UILabel!
+    
     var nowDate: Date?
     
     override func viewDidLoad() {
@@ -25,6 +30,7 @@ class AnniversaryViewController: UIViewController {
     func configureUI() {
         
         datePickerView.preferredDatePickerStyle = .wheels
+        datePickerView.datePickerMode = .date
         
         imageViewCollections.forEach{$0.layer.cornerRadius = 25}
         imageViewCollections.forEach{$0.layer.masksToBounds = true }
@@ -37,18 +43,38 @@ class AnniversaryViewController: UIViewController {
         uiViewCollections.forEach{$0.layer.masksToBounds = false }
     }
     
-    
     @IBAction func datePickerViewAction(_ sender: Any) {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.dateStyle = .full
         generateAndApplyDate(formatter)
+        make100Date(formatter)
+        make200Date(formatter)
+        make300Date(formatter)
+        make400Date(formatter)
     }
     
     func generateAndApplyDate(_ formatter: DateFormatter) {
         nowDate = datePickerView.date
         testLabel.text = formatter.string(from: nowDate!)
     }
+    func make100Date(_ formatter: DateFormatter) {
+        nowDate = datePickerView.date + 86400*100
+        later100Label.text = formatter.string(from: nowDate!)
+    }
+    func make200Date(_ formatter: DateFormatter) {
+        nowDate = datePickerView.date + 86400*200
+        later200Label.text = formatter.string(from: nowDate!)
+    }
+    func make300Date(_ formatter: DateFormatter) {
+        nowDate = datePickerView.date + 86400*300
+        later300Label.text = formatter.string(from: nowDate!)
+    }
+    func make400Date(_ formatter: DateFormatter) {
+        nowDate = datePickerView.date + 86400*400
+        later400Label.text = formatter.string(from: nowDate!)
+    }
+    
 }
 
 
