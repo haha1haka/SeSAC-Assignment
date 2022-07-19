be08f016d93d3d8c2581db3b7a72cceabfbb132c
haha1haka <haha1haka@icloud.com>
2022-07-19 18:20:15 +0900

    [#6] 012회차 과제: 각cell radius 주기 첫번째 방법 UI구성(containerView 추가)

--- TrendMedia/TrendMedia/Sources/Scenes/Shopping/ShoppingTableViewController.swift
+++ TrendMedia/TrendMedia/Sources/Scenes/Shopping/ShoppingTableViewController.swift
@@ -8,17 +8,19 @@
 import UIKit
 
 class ShoppingTableViewController: UITableViewController {
-
     
     @IBOutlet weak var addTextField: UITextField!
     @IBOutlet weak var addButton: UIButton!
     
-        
+    var shoppingList: [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
+    
     func configure() {
+        tableView.separatorInset = .init()
         addTextField.layer.cornerRadius = 8
         addTextField.backgroundColor = UIColor.systemGray6
         addTextField.placeholder = "무엇을 구매하실 건가요?"
@@ -28,5 +30,50 @@ class ShoppingTableViewController: UITableViewController {
         addButton.tintColor = .black
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell", for: indexPath) as! ShoppingTableViewCell
+        
+        cell.selectionStyle = .none
+        
+        
+        cell.checkBoxButton.tintColor = .label
+        cell.starButton.tintColor = .label
+        
+        
+        cell.containerView.layer.cornerRadius = 8
+        cell.containerView.backgroundColor = .systemGray6
+        
+        cell.checkBoxButton.tintColor = .label
+        cell.starButton.tintColor = .label
+        
+        return cell
+    }
+    
+    
+    @IBAction func addTextFieldReturn(_ sender: UITextField) {
+        shoppingList.append(sender.text ?? "")
+        tableView.reloadData()
+    }
+    
+    
+    
+    
+    
     
 }
+
+
+
+
+
+
+
+
+//
+//        cell.containerView.backgroundColor = .systemGray6
+//        cell.containerView.layer.cornerRadius = 8
+
+
--- TrendMedia/TrendMedia/Sources/Scenes/Shopping/Views/ShoppingTableViewCell.swift
+++ TrendMedia/TrendMedia/Sources/Scenes/Shopping/Views/ShoppingTableViewCell.swift
@@ -8,16 +8,14 @@
 import UIKit
 
 class ShoppingTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    
+    
+    
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var starButton: UIButton!
+    
 }
